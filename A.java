5ebb7cda43f09740c40c366304d0eef755d30525
harshavardhangadde <60385597+harshavardhangadde@users.noreply.github.com>
2020-01-28 15:39:10 +0530

    command line arguments

---
+++ A.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+class A
+
+{
+
+ public static void main(String args[])
+
+ {
+
+System.out.println(args[0]+ "technologies"+args[1]);}}
+/*
+System.out.println("Welcome"+args[0]);
+int sum=0,x,y;
+x=Integer.parseInt(args[0]);
+y=Integer.parseInt(args[1]);
+ sum=x+y;
+System.out.println(sum);
+
+int n=0;
+
+ if(n>0)
+{
+ System.out.println("The given number is Positive");
+}
+else if(n<0)
+{
+  System.out.println("The given number is Negative");
+}
+     else
+        {
+            System.out.println("The given number is neither Positive nor Negative ");
+}
+}
+}
+
+int i,n,count = 0; 
+		Scanner sc = new Scanner(System.in);	
+		n= sc.nextInt();	
+		for (i = 2;i <=n/2;i++)
+		{
+			if(n%i == 0)
+		    {
+                       count++;	
+		        break;
+		    }	
+		}
+if(n<0)
+{
+System.out.println("Enter integer value only");
+}
+		else if(count == 0)
+		{
+			System.out.println("Given number is a Prime Number");
+		}
+		else
+		{
+		   System.out.println("Given number is Not a Prime Number");
+		}
+}
+}
+*/
